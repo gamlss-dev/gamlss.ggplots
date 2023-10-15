6151a21b98a473cee47f509c6442d1e53f94a119
Mikis Stasinopoulos <dmh.stasinopoulos@gmail.com>
2023-10-15 22:30:27 +0100

    test

--- R/data_functions.R
+++ R/data_functions.R
@@ -10,8 +10,8 @@
 #  NOT THIS ONE 1) data_check       : will be fixed in the end 
 ################################################################################
 #  3) data_response    :  
-#  5) xy_power_trans         : 
-# 14) y_power_trans     : takes x and y and find power transformation for x
+#  5) xy_power_trans   : 
+# 14) y_power_trans    : takes x and y and find power transformation for x
 # 16) datetime2datehour: from datetime to date and hour
 # 18) y_zscores        : transform one variable to SHASH or other dist. z-scores 
 # 19) y_outliers       : identify outliers of one x by taking z-scores
@@ -403,4 +403,7 @@ if (is(data, "array"))
      da <- subset(data, select=CData!=class)
 da
 }  
-
+################################################################################
+################################################################################
+################################################################################
+################################################################################
